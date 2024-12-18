2a7939aa308fd2872f528594372053d1f72cdcc3
WMKC_WIJESUNDARA <wmkcwijesundara@students.nsbm.ac.lk>
2024-12-18 23:43:54 +0530

    minor changes

--- admin/index.php
+++ admin/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
+    
     <!--bootsrap css link-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
@@ -13,6 +14,11 @@
 
     <div class="wrapper">
         <aside id="sidebar">
+            <div class="d-flex">
+                <button id="toggle-btn" type="button">
+                <i class="fa-solid fa-bars"></i>
+                </button>
+            </div>
             <ul class="sidebar-nav">
                 <li class="sidebar-item">
                     <a href="#" class="sidebar-link">
@@ -58,11 +64,26 @@
                 </li>
             </ul>
         </aside>
+        <div class="main">
+            <nav class="navbar navbar-expand px-4 py-3">
+                <form action="#" class="d-none d-sm-inline-block">
+                    <div class="input-group input-group-navbar">
+                        <input type="text" class="form-control">
+                    </div>
+                </form>
+                <div class="navbar-collapse collapse">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item dropdown"></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </div>
 
     <!--bootsrap js link-->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
 </body>
 </html>
---
+++ admin/script.js
@@ -0,0 +1,5 @@
+const testerbeta = document.querySelector("#toggle-btn");
+
+testerbeta.addEventListener("click",function(){
+    document.querySelector("#sidebar").classList.toggle("expand");
+});
